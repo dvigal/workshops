b0b46bf98c1fd7ff651211d114ab636dce0f3678
Alexander Litvinov <1299144+dvigal@users.noreply.github.com>
2022-07-03 10:41:12 +0300

    Минимальные примеры кода Kafka Producer/Consumer

--- src/main/java/practicum/kafka/Producer.java
+++ src/main/java/practicum/kafka/Producer.java
@@ -3,6 +3,7 @@ package practicum.kafka;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -21,11 +22,16 @@ public class Producer {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+
         try (var producer = new KafkaProducer<String, String>(props)) {
             var metadata = producer.send(new ProducerRecord<>(TOPIC_NAME, "Hello, World!")).get();
-            System.out.println(metadata);
+            System.out.println(asString(metadata));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static String asString(RecordMetadata metadata) {
+        return "[topic=" + metadata.topic() + ", partition=" + metadata.partition() + ", offset=" + metadata.offset() + "]";
     }
 }
